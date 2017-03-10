b09be6ae64ebba1581ca6e0478290dc2c14e40d1
Colin Petruno <colinpetruno@gmail.com>
2017-03-09 21:29:36 -0500

    User Auth and Devise Layout
    Now that a user is able to be signed in I wanted to ensure that the
    internal application checks their authentication. I added the provided
    devise method to the authenticated controller.
    
    The Login page was then on the wrong layout so I used the devise custom
    base class to ensure that we use the right layout for the devise views.
    
    https://github.com/plataformatec/devise/wiki/How-To:-Create-custom-layouts

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -1,6 +1,7 @@
 class AuthenticatedController < ApplicationController
   layout "application"
 
+  before_action :authenticate_user!
   helper_method :current_company
 
   def current_company
---
+++ app/controllers/devise_base_controller.rb
@@ -0,0 +1,3 @@
+class DeviseBaseController < ActionController::Base
+  layout "marketing"
+end
