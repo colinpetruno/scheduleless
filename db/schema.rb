dbdf56c9d42e779de22aa141f8a1993a4cd24dd6
Colin Petruno <colinpetruno@gmail.com>
2017-03-18 12:15:16 -0400

    Commit Schema Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304033940) do
+ActiveRecord::Schema.define(version: 20170318143818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,10 +45,8 @@ ActiveRecord::Schema.define(version: 20170304033940) do
   create_table "preferred_hours", force: :cascade do |t|
     t.integer "user_id"
     t.integer "day"
-    t.time    "start"
-    t.time    "end"
-    t.boolean "open",    default: true
-    t.boolean "close",   default: true
+    t.integer "start",   default: 0,    null: false
+    t.integer "end",     default: 1440, null: false
     t.index ["user_id", "day"], name: "index_preferred_hours_on_user_id_and_day", unique: true, using: :btree
     t.index ["user_id"], name: "index_preferred_hours_on_user_id", using: :btree
   end
