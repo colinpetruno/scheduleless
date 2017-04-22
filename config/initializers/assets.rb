9f7d90633f6a4cd086dd919cc488d58f266e391d
Colin Petruno <colinpetruno@gmail.com>
2017-04-22 11:43:53 -0400

    Set Up Home Page Manifests

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,8 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(
+  marketing.css
+  marketing.js
   onboarding.css
   onboarding.js
 )
