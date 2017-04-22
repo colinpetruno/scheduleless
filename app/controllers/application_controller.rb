f6f4e4258da1521d48d660ab1bb0be83137c7f7f
Colin Petruno <colinpetruno@gmail.com>
2017-04-22 13:55:15 -0400

    Marketing and User Flows
    Add a marketing basic layout. Much improvement here yet but this
    makes log out and login links work. Hitting the marketing
    site while logged in will now direct you into the application
    automatically. The after login url also was updated to prevent
    a double redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   include Pundit
 
+  def after_sign_in_path_for(resource)
+    request.env['omniauth.origin'] || stored_location_for(resource) || calendar_path
+  end
+
   protect_from_forgery with: :exception
 end
--- app/controllers/marketing/welcome_controller.rb
+++ app/controllers/marketing/welcome_controller.rb
@@ -1,6 +1,16 @@
 class Marketing::WelcomeController < ApplicationController
   layout "marketing"
 
+  before_filter :check_signed_in_user
+
   def index
+  end
+
+  private
+
+  def check_signed_in_user
+    if current_user.present?
+      redirect_to calendar_path and return
+    end
   end
 end
