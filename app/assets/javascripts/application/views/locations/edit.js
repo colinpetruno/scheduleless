664d815838a42b2b8ed33d3843a7af696f41e40f
Colin Petruno <colinpetruno@gmail.com>
2017-04-23 18:09:12 -0400

    Prevent Javascript Error

--- app/assets/javascripts/application/views/locations/edit.js
+++ app/assets/javascripts/application/views/locations/edit.js
@@ -1,29 +1,31 @@
 $(document).on("turbolinks:load", function() {
-  var available_employees = new Bloodhound({
-    datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
-    queryTokenizer: Bloodhound.tokenizers.whitespace,
-    remote: {
-      url: $("#location-available-employees").data("url"),
-      wildcard: 'QUERY'
-    }
-  });
+  if($('#location-available-employees').length) {
+    var available_employees = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      remote: {
+        url: $("#location-available-employees").data("url"),
+        wildcard: 'QUERY'
+      }
+    });
 
-  $('#location-available-employees').typeahead(null, {
-    hint: true,
-    name: 'employees',
-    display: 'email',
-    limit: 10,
-    source: available_employees,
-    templates: {
-      empty: [
-        '<div class="empty-message">',
-          'No employees found',
-        '</div>'
-      ].join('\n')
-    }
-  });
+    $('#location-available-employees').typeahead(null, {
+      hint: true,
+      name: 'employees',
+      display: 'email',
+      limit: 10,
+      source: available_employees,
+      templates: {
+        empty: [
+          '<div class="empty-message">',
+            'No employees found',
+          '</div>'
+        ].join('\n')
+      }
+    });
 
-  $('#location-available-employees').bind('typeahead:selected', function(obj, datum, name) {
-    $("#user_location_user_id").val(datum.id)
-  });
+    $('#location-available-employees').bind('typeahead:selected', function(obj, datum, name) {
+      $("#user_location_user_id").val(datum.id)
+    });
+  }
 });
