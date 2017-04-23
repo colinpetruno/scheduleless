928eea10a873e19a1c84523c82d3ea94b9d08d8a
Chris Jennison <cjennison92@gmail.com>
2017-04-23 12:02:36 -0400

    Scheduling Constraints 1) Max schedule constraints are now used to limit shift lengths. This is done by assuming the timeslot is assigned and counting up and down timeslots and counting the number of consecutive timeslots then multiplying by minutes/interval 2) Fixed initial assignment loop. This would just timeout. Now it re-selects the next slot correctly and considers positions from a sample of the employees positions 3) Added Position Scheduling. Positions are not incorporated into timeslots and eligibility. Timeslots are aware of available timeslots for a position and assigners are sure to check these position slots before assigning.

--- app/models/scheduler/layout_generator.rb
+++ app/models/scheduler/layout_generator.rb
@@ -1,13 +1,14 @@
 module Scheduler
   class LayoutGenerator
-    def self.for(schedule, options)
-      new(schedule: schedule, options: options).generate
+    def self.for(schedule, rules, options)
+      new(schedule: schedule, rules: rules, options: options).generate
     end
 
-    def initialize(schedule:, layout_class: Scheduler::Layout, options: Options.new)
+    def initialize(schedule:, layout_class: Scheduler::Layout, rules:, options: Options.new)
       @schedule = schedule
       @layout_class = layout_class
       @options = options
+      @rules = rules
     end
 
     def generate
@@ -15,7 +16,7 @@ module Scheduler
         day = []
 
         (0..options.number_of_intervals).each do |interval_number|
-          day.push(Timeslot.new(day_number, interval_number, shift_allotment))
+          day.push(Timeslot.new(day_number, interval_number, shift_allotment(interval_number), rules, options))
         end
 
         layout.add_day(day)
@@ -26,18 +27,24 @@ module Scheduler
 
     private
 
-    def shift_allotment
-      if options.allow_zero_shift
-        0 + rand(options.shift_range + 1)
-      else
-        1 + rand(options.shift_range)
+    def shift_allotment(interval_number)
+      allocation = 0
+      rules.each do |rule|
+        allocation = allocation + rule.number_of_employees if rule_within_bounds(rule.period, interval_number)
       end
+      allocation
+    end
+
+    def rule_within_bounds(rule_period, interval)
+      interval_minutes = interval*options.time_interval_minutes
+      rule_timeframe = options.period_timeframes(rule_period)
+      interval_minutes >= rule_timeframe[:start] && interval_minutes < rule_timeframe[:end]
     end
 
     def layout
       @_layout ||= layout_class.new(options)
     end
 
-    attr_reader :options, :layout_class, :schedule
+    attr_reader :options, :layout_class, :schedule, :rules
   end
 end
