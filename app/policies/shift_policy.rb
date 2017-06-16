ca35182963472416ce409380c627190cbf212ba8
Colin Petruno <colinpetruno@gmail.com>
2017-06-15 23:47:53 -0400

    Some Updates

--- app/policies/shift_policy.rb
+++ app/policies/shift_policy.rb
@@ -6,22 +6,28 @@ class ShiftPolicy < ApplicationPolicy
   end
 
   def edit?
-    user.company_admin?
+    user.company_admin? || location_admin_for?(shift_location)
   end
 
   def new?
-    user.company_admin?
+    user.company_admin? || location_admin_for?(shift_location)
   end
 
   def create?
-    user.company_admin?
+    user.company_admin? || location_admin_for?(shift_location)
   end
 
   def destroy?
-    user.company_admin?
+    user.company_admin? || location_admin_for?(shift_location)
   end
 
   def update?
-    user.company_admin?
+    user.company_admin? || location_admin_for?(shift_location)
+  end
+
+  private
+
+  def shift_location
+    record.location
   end
 end
