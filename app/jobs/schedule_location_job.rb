5ca3621c84880e26318eb0d672a8c0ed489e5d69
Colin Petruno <colinpetruno@gmail.com>
2017-06-17 10:15:48 -0400

    Skip Scheduleless Approval for Demos
    We don't want the approvals section to get clogged with
    demo accounts, this skips the scheduleless review in the
    case where the account is marked as a demo and moves it
    right into company review.

--- app/jobs/schedule_location_job.rb
+++ app/jobs/schedule_location_job.rb
@@ -3,18 +3,28 @@ class ScheduleLocationJob < ApplicationJob
 
   def perform(schedule_period_id)
     @scheduling_period = SchedulingPeriod.find(schedule_period_id)
-    company = @scheduling_period.company
-    schedule_setting = company.schedule_setting
 
     if scheduleable?
       scheduler = Scheduler::Schedule.for(@scheduling_period)
       scheduler.generate
     end
 
-    @scheduling_period.update(status: :generated)
+    @scheduling_period.update(status: completed_status)
   end
 
   private
+
+  def company
+    @scheduling_period.company
+  end
+
+  def completed_status
+    if company.demo?
+      :scheduleless_approved
+    else
+      :generated
+    end
+  end
 
   def location
     @scheduling_period.location
