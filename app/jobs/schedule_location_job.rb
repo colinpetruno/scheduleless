e1340d18af7bb4a3897bc2cf0bc4b27d1a54ad72
Colin Petruno <colinpetruno@gmail.com>
2017-06-16 21:41:17 -0400

    Harden Algorithm Job
    At points in the algorithm its not accounting for nil users.
    This hardens it at the job level for now

--- app/jobs/schedule_location_job.rb
+++ app/jobs/schedule_location_job.rb
@@ -2,14 +2,26 @@ class ScheduleLocationJob < ApplicationJob
   queue_as :default
 
   def perform(schedule_period_id)
-    scheduling_period = SchedulingPeriod.find(schedule_period_id)
-    company = scheduling_period.company
+    @scheduling_period = SchedulingPeriod.find(schedule_period_id)
+    company = @scheduling_period.company
     schedule_setting = company.schedule_setting
 
-    scheduler = Scheduler::Schedule.for(scheduling_period)
+    if scheduleable?
+      scheduler = Scheduler::Schedule.for(@scheduling_period)
+      scheduler.generate
+    end
 
-    scheduler.generate
+    @scheduling_period.update(status: :generated)
+  end
 
-    scheduling_period.update(status: :generated)
+  private
+
+  def location
+    @scheduling_period.location
+  end
+
+  def scheduleable?
+    # TODO: can add more error checking in here for known failure cases
+    location.users.exists?
   end
 end
