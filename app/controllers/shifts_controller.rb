d5fdd6e07b2ab28551c59b84184e70924ace531a
Colin Petruno <colinpetruno@gmail.com>
2017-06-24 11:51:35 -0400

    Finish Day Schedule Editing

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -3,49 +3,6 @@ class ShiftsController < AuthenticatedController
     @shifts = policy_scope(Shift)
   end
 
-  def edit
-    @location = current_company.locations.find(params[:location_id])
-    @shift = @location.shifts.find(params[:id])
-    @users = @location.users
-
-    authorize @shift
-  end
-
-  def update
-    @location = current_company.locations.find(params[:location_id])
-    @shift = @location.shifts.find(params[:id])
-
-    authorize @shift
-
-    if @shift.update(shift_params)
-      redirect_to location_calendar_path(@location), notice: t("shifts.edit.notice")
-    else
-      redirect_to location_calendar_path(@location), alert: t("shifts.edit.notice")
-    end
-
-  end
-
-  def new
-    date = Date.parse(params[:date]).strftime('%Y%m%d').to_i
-    @location = current_company.locations.find(params[:location_id])
-    @shift = @location.shifts.build(date: date)
-    @users = @location.users
-
-    authorize @shift
-  end
-
-  def destroy
-    @location = current_company.locations.find(params[:location_id])
-    @shift = @location.shifts.find(params[:id])
-    authorize @shift
-
-    if @shift.destroy
-      redirect_to location_calendar_path(@location), notice: t("shifts.destroy.notice")
-    else
-      redirect_to location_calendar_path(@location), alert: t("shifts.destroy.alert")
-    end
-  end
-
   private
 
   def shift_params
