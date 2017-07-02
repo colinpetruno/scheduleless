dc1eb64f710da2832cf6bebdf93f821314651cbc
Colin Petruno <colinpetruno@gmail.com>
2017-07-02 13:41:12 -0400

    Schedule Published Inviter Bugfixes
    Apparently resque can't use a symbol as an argument.
    Also selecting distinct users so people don't get double notified.

--- app/jobs/schedule_published_inviter_job.rb
+++ app/jobs/schedule_published_inviter_job.rb
@@ -5,7 +5,8 @@ class SchedulePublishedInviterJob < ApplicationJob
     @scheduling_period = SchedulingPeriod.find(schedule_period_id)
     @users = User.
       joins(:in_progress_shifts).
-      where(in_progress_shifts: { scheduling_period_id: schedule_period_id })
+      where(in_progress_shifts: { scheduling_period_id: schedule_period_id }).
+      distinct
 
     @users.map do |user|
       if user.invitation_state == :awaiting_invite
@@ -13,7 +14,7 @@ class SchedulePublishedInviterJob < ApplicationJob
       elsif user.invitation_state == :active
         begin
           PushNotificationSenderJob.
-            perform_later(user.id, :schedule_published)
+            perform_later(user.id, "schedule_published")
 
           NotificationsMailer.
             schedule_published(user, @scheduling_period).
