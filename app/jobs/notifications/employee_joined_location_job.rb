49d1bc7a1888c7d74b31b644788b5b0acf667093
Colin Petruno <colinpetruno@gmail.com>
2017-07-02 14:36:02 -0400

    Fix Typo

--- app/jobs/notifications/employee_joined_location_job.rb
+++ app/jobs/notifications/employee_joined_location_job.rb
@@ -9,7 +9,7 @@ module Notifications
       users_to_alert.map do |user|
         begin
           # send email
-          NotificationMailer.
+          NotificationsMailer.
             employee_joined_location(user, @user, @location).
             deliver
 
