163a12e91c36522ebed8241fd30a7331f80299a9
Colin Petruno <colinpetruno@gmail.com>
2017-07-08 18:59:35 -0400

    Set Cache Control Headers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,6 +71,10 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.public_file_server.headers = {
+    "Cache-Control": "public, s-maxage=31536000, maxage=15552000",
+    "Expires": "#{1.year.from_now.to_formatted_s(:rfc822)}"
+  }
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
