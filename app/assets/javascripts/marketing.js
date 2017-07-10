c90116d65585c5f40e2e31e5d3a3848ceb0bbe86
Colin Petruno <colinpetruno@gmail.com>
2017-07-10 12:07:27 -0400

    Fix Tracking

--- app/assets/javascripts/marketing.js
+++ app/assets/javascripts/marketing.js
@@ -1,5 +1,7 @@
 //= require jquery
 //= require jquery_ujs
+// we cant remove turbolinks or tracking breaks
+//= require turbolinks
 //= require_tree ./tracking
 //= require tether
 //= require bootstrap-sprockets
