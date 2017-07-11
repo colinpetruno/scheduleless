70dcbc5773d967b5ee37c5beabccd7e9ceaff30e
Colin Petruno <colinpetruno@gmail.com>
2017-07-11 10:41:42 -0400

    Add Size to Companies
    Mainly for curiousity sake to see how big
    these things are in case people bounce

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710234417) do
+ActiveRecord::Schema.define(version: 20170711143644) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20170710234417) do
     t.string   "stripe_customer_id"
     t.boolean  "demo",               default: false, null: false
     t.string   "hash_key"
+    t.string   "size"
   end
 
   create_table "credit_cards", force: :cascade do |t|
