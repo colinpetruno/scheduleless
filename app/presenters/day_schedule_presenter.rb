c2ca81634b9153c4bba460fecb96a374d28ef36a
Chris Jennison <cjennison92@gmail.com>
2017-07-15 12:51:37 -0400

    Added support for overnight shift display

--- app/presenters/day_schedule_presenter.rb
+++ app/presenters/day_schedule_presenter.rb
@@ -33,10 +33,6 @@ class DaySchedulePresenter
   end
 
   def end_hour
-
-    last_shift = shifts.sort_by{ |obj| obj.minute_end }.last
-    last_shift_minute = last_shift.minute_end if last_shift
-
     if !last_shift_minute
       nil
     else
@@ -44,6 +40,7 @@ class DaySchedulePresenter
         hours.minute_schedulable_end,
         last_shift_minute
       ].sort.last
+
       MinutesToTime.new(minutes: end_time).next_hour
     end
   end
@@ -66,9 +63,6 @@ class DaySchedulePresenter
   end
 
   def start_hour
-    first_shift = shifts.sort_by{ |obj| obj.minute_start }.first
-    first_shift_minute = first_shift.minute_start if first_shift
-
     if !first_shift_minute
       nil
     else
@@ -79,26 +73,75 @@ class DaySchedulePresenter
 
       MinutesToTime.new(minutes: start_time).start_of_hour
     end
+  end
 
+  def next_day
+    (day_date + 1.day).strftime('%m/%d/%Y')
   end
 
   def shift_style(shift)
     if start_hour
+
+      shift_width = shift.minute_end - shift.minute_start
+      if shift.minute_end < shift.minute_start
+        shift_width = overnight_minute_end(shift.minute_end) - shift.minute_start
+      end
+
       left = ((shift.minute_start / 15) - (start_hour.hour * 60 / 15)) * 10
-      width = (shift.minute_end - shift.minute_start) / 15 * 10
+      width = (shift_width) / 15 * 10
 
       "left: #{left}px; width: #{width}px"
     else
       "left: 0px; width: 0px"
     end
-
   end
-
 
   private
 
   def day_date
     Date.parse(day.to_s)
+  end
+
+  def overnight_minute_end(mod_minute)
+    mod_minute + (24*60)
+  end
+
+  def first_shift_minute
+    first_shift = shifts.sort_by{ |obj| obj.minute_start }.first
+    first_shift.minute_start if first_shift
+  end
+
+  def last_shift_minute
+    last_shift = find_latest_shift #shifts.sort_by{ |obj| obj.minute_end }.last
+    minute = last_shift.minute_end if last_shift
+
+    if minute < last_shift.minute_start
+      minute = overnight_minute_end(minute)
+    end
+
+    minute
+  end
+
+  def find_latest_shift
+    latest = shifts.first
+    latest_shift_end_minute = latest.minute_end
+    if latest.minute_end < latest.minute_start
+      latest_shift_end = overnight_minute_end(latest.minute_end)
+    end
+
+    shifts.each do |shift|
+      shift_end = shift.minute_end
+      if shift.minute_end < shift.minute_start
+        shift_end = overnight_minute_end(shift_end)
+      end
+
+      if latest_shift_end_minute < shift_end
+        latest_shift_end_minute = shift_end
+        latest = shift
+      end
+    end
+
+    latest
   end
 
   def day_integer
