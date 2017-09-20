2091446a5d1278e91c9248a7d73a66f9aaa7168d
Colin Petruno <colinpetruno@gmail.com>
2017-09-20 11:35:49 -0400

    Proper Updating of Repeating Shifts
    Carry the changes through to ensure that repeating shifts get properly
    edited in the in progress shifts table as well as the preview values
    set.

--- app/models/shifts/repeating_shifts_updater.rb
+++ app/models/shifts/repeating_shifts_updater.rb
@@ -10,24 +10,36 @@ module Shifts
 
     def update(params)
       ActiveRecord::Base.transaction do
-        repeating_shift.update(params)
+        repeating_shift.update(preview_position_id: params[:position_id],
+                               preview_user_id: params[:user_id],
+                               repeat_frequency: params[:repeat_frequency])
 
 
         seeded_shifts.each do |shift|
           if invalid?(shift)
-            # delete shift
+            # delete the inprogress shift out of the database, this is so
+            # if the shift changes back to an old frequency that the shift
+            # can get repopulated
+            shift.delete
           else
             Shifts::Updater.
-              for(shift).
-              update(position_id: repeating_shift.position_id,
-                     user_id: repeating_shift.user_id)
+              update(
+                shift,
+                {
+                  minute_start: repeating_shift.preview_minute_start,
+                  minute_end: repeating_shift.preview_minute_end,
+                  position_id: repeating_shift.preview_position_id,
+                  user_id: repeating_shift.preview_user_id
+                }
+              )
           end
         end
 
 
       end
     rescue StandardError => error
-      Bugsnag.report(error)
+      binding.pry
+      Bugsnag.notify(error)
       false
     end
 
@@ -36,10 +48,10 @@ module Shifts
     attr_reader :repeating_shift
 
     def seeded_shifts
-      ShiftInProgress.where(repeating_shift_id: repeating_shift.id)
+      InProgressShift.where(repeating_shift_id: repeating_shift.id)
     end
 
-    def valid?(shift)
+    def invalid?(shift)
       offset = shift.date - repeating_shift.start_date
 
       offset % repeating_shift.repeat_frequency != 0
