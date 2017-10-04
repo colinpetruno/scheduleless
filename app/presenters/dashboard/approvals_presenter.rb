bc909d35aeb6554c66f2ad570aa254403dd6f691
Colin Petruno <colinpetruno@gmail.com>
2017-10-04 00:17:28 -0400

    Start Hooking Up Approvals Dashboard Tile
    Luckily the only thing to approve at the moment is shift trades.
    Although time off requests could be a possibility as well...

---
+++ app/presenters/dashboard/approvals_presenter.rb
@@ -0,0 +1,67 @@
+module Dashboard
+  class ApprovalsPresenter
+    def initialize(user)
+      @user = user
+    end
+
+    def trade_approvals?
+      offers.present?
+    end
+
+    def trade_approvals
+      trade_map
+    end
+
+    def trade_for(id)
+      trades.to_a.select { |t| t.id == id }.first
+    end
+
+    private
+
+    attr_reader :user
+
+    def build_trade_map
+      map = {}
+
+      offers.map do |offer|
+        if map[offer.trade_id].present?
+          map[offer.trade_id].push(offer)
+        else
+          map[offer.trade_id] = [offer]
+        end
+      end
+
+      map
+    end
+
+    def location_ids
+      locations.pluck(:id)
+    end
+
+    def locations
+      if user_permissions.company_admin?
+        @_locations ||= user.company.locations
+      else
+        @_locations ||= user.locations
+      end
+    end
+
+    def offers
+      @_offers ||= Offer.
+        joins(:trade).
+        where(trades: { location_id: location_ids }, state: :waiting_approval)
+    end
+
+    def trades
+      @_trades ||= Trade.where(id: offers.map(&:trade_id)).includes(:shift)
+    end
+
+    def trade_map
+      @_trade_map ||= build_trade_map
+    end
+
+    def user_permissions
+      @_permissions = UserPermissions.for(user)
+    end
+  end
+end
