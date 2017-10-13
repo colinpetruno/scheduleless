c98fc65c8736cd121dc3be6b22adf445bd59bc0b
Colin Petruno <colinpetruno@gmail.com>
2017-10-12 22:51:53 -0400

    Useability Tweaks on Calendar
    - Pushing escape will close the sidebar.
    - A modal to show the join link and give a brief explanation of how it
    works
    - A link to the employees page directly from the calendar

--- app/assets/javascripts/application/components/tooltip.js
+++ app/assets/javascripts/application/components/tooltip.js
@@ -1,3 +1,7 @@
 $(document).on("turbolinks:load", function() {
   $('[data-toggle="tooltip"]').tooltip();
 });
+
+$(document).on("turbolinks:before-visit", function() {
+  $('[data-toggle="tooltip"]').tooltip('dispose');
+});
