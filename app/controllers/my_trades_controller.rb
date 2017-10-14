bea10e46478ec6cf2d0aa581834a74652a223ea7
Chris Jennison <cjennison92@gmail.com>
2017-10-14 13:56:44 -0400

    Dont show trades of shifts that have already started

--- app/controllers/my_trades_controller.rb
+++ app/controllers/my_trades_controller.rb
@@ -2,6 +2,33 @@ class MyTradesController < AuthenticatedController
   def show
     authorize :my_trade
 
-    @trades = current_user.trades.includes(:offers, :shift).available
+    base_trades = current_user.trades.includes(:offers, :shift)
+    @trades = base_trades.
+      where(
+        shifts: {
+          date: ((location_date+1)..Float::INFINITY)
+        }
+      ).or(
+        base_trades.where(
+          shifts: {
+            date: location_date,
+            minute_start: (location_time_minutes..Float::INFINITY)
+          }
+        )
+      ).available
+  end
+
+  private
+
+  def location_date
+    DateAndTime::LocationTime.
+      new(location: current_user.locations.first).
+      current_date_integer
+  end
+
+  def location_time_minutes
+    DateAndTime::LocationTime.
+      new(location: current_user.locations.first).
+      current_time_minutes
   end
 end
