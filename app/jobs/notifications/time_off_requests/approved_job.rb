fe693cc4cc7eaff4fd9fe2a58bb62afe7e21322f
Colin Petruno <colinpetruno@gmail.com>
2017-10-18 21:18:58 -0400

    Fix Syntax Errors

--- app/jobs/notifications/time_off_requests/approved_job.rb
+++ app/jobs/notifications/time_off_requests/approved_job.rb
@@ -3,16 +3,16 @@ module Notifications
     class ApprovedJob < ApplicationJob
       def perform(time_off_request_id)
         @time_off_request = TimeOffRequest.find(time_off_request_id)
-        user = @time_off_request.user
+        @user = @time_off_request.user
 
         begin
           PushNotifications::TimeOffRequests::Approved.
-              new(user: user, time_off_request: time_off_request).
+              new(user: @user, time_off_request: @time_off_request).
               notify
 
-          if Users::Emailable.for(user)
+          if Users::Emailable.for(@user)
             NotificationsMailer.
-              time_off_request_denied(user, time_off_request).
+              time_off_request_denied(@user, @time_off_request).
               deliver
           end
         rescue StandardError => error
