fc350bc2e50c88b2449a7ae8c82ec882a03ae52a
Colin Petruno <colinpetruno@gmail.com>
2017-10-30 17:41:53 -0400

    Bugfix: Use User instead of LoginUser afterall
    I queued the user id and there is a failed job in the queue.

--- app/jobs/new_customer_job.rb
+++ app/jobs/new_customer_job.rb
@@ -2,7 +2,8 @@ class NewCustomerJob < ApplicationJob
   queue_as :default
 
   def perform(user_id)
-    @user = LoginUser.find(user_id).user
+    # TODO: Update this to use LoginUser instead of user
+    @user = User.find(user_id)
 
     if Users::Emailable.for(@user)
       WelcomeDripMailer.welcome(@user).deliver
