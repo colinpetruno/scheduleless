8de649a2d7039ef6a3c83bce01e29d7df2a8e818
Chris Jennison <cjennison92@gmail.com>
2017-12-10 13:48:17 -0500

    Added adapter to sitemapper

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,4 +1,15 @@
+
 SitemapGenerator::Sitemap.default_host = "http://www.coworkability.io"
+SitemapGenerator::Sitemap.public_path = 'tmp/'
+
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(fog_provider: 'AWS',
+                                         aws_access_key_id: ENV["S3_ACCESS_KEY"],
+                                         aws_secret_access_key: ENV["S3_SECRET"],
+                                         fog_directory: ENV["BUCKETNAME_SITEMAPS"],
+                                         fog_region: 'us-east-2')
+
+SitemapGenerator::Sitemap.sitemaps_host = "http://s3.us-east-2.amazonaws.com/#{ENV["BUCKETNAME_SITEMAPS"]}/"
+
 SitemapGenerator::Sitemap.create do
   add why_report_path
 
